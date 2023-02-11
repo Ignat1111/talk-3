e5f086c1220cf6af3b1f9119d847a866f21184d7
Ignat lazinkov <lazinkovignat@gmail.com>
2023-02-11 22:26:53 +0300

    Даполнил задачи

---
+++ Talk_21/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 21: Напишите программу, которая принимает 
+// на вход координаты двух точек и находит расстояние 
+// между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+Console.WriteLine("Введите x1,y1,z1: ");
+double x1 = double.Parse(Console.ReadLine()!);
+double y1 = double.Parse(Console.ReadLine()!);
+double z1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите x2,y2,z2: ");
+double x2 = double.Parse(Console.ReadLine()!);
+double y2 = double.Parse(Console.ReadLine()!);
+double z2 = double.Parse(Console.ReadLine()!);
+
+double res = Math.Round(Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2) + Math.Pow(z1-z2, 2)), 2);
+
+Console.WriteLine($"Расстояние между тремя точками: {res}");
---
+++ Talk_23/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+// public static TSelf Cbrt (TSelf x);
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int cube = int.Parse(Console.ReadLine()!);
+
+int count = 1;
+
+while (count <= cube)
+{
+   Console.Write(count * count * count);
+   
+   if (count != cube) Console.Write(", ");
+   count++;
+}
